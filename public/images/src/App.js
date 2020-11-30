2114e9413f4f405aaeebcc72554383395802f70f
Monica Verdi <monica.verdi@gmail.com>
2020-11-30 20:47:37 +0100

    open file app.js and index.html

---
+++ public/images/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import 'App.css';
+
+class App extends React.Component{
+    render({
+        return(
+            <div>
+                <div className="main-header" >
+                    <img src = />
+                </div>
+
+                <h1>Say hello to ReactJS</h1>
+                <p>You will learn how to use the most popular frontend library, and become a super Ninja developer</p>
+            </div>
+        )
+    })
+
+}
+
